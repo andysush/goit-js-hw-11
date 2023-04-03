7388f9a9bd5088c29b19df9598913bd87ca9cea1
Andrii <sushkoandy1@gmail.com>
2023-04-03 15:43:25 +0300

    all with loadMoreBtn

--- src/getPhotoApi.js
+++ src/getPhotoApi.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { Notify } from 'notiflix';
 export default class PhotoApiService {
   constructor() {
     this.API_KEY = '34883427-fe70cd3747fe88c31215abbc1';
@@ -8,25 +9,26 @@ export default class PhotoApiService {
     this.per_page = 40;
   }
 
-  getPhotos() {
-    const searchParams = new URLSearchParams({
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-      page: this.page,
-      per_page: this.per_page,
-    });
-
-    return fetch(
-      `${this.BASE_URL}?key=${this.API_KEY}&q=${this.searchQuery}&${searchParams}`
-    )
-      .then(response => response.json())
-      .then(data => {
-        this.incrementPageNum();
-
-        return data;
-      });
-  }
+  getPhotos = async () => {
+    try {
+      const { data } = await axios.get(
+        `${this.BASE_URL}?key=${this.API_KEY}&q=${this.searchQuery}`,
+        {
+          params: {
+            image_type: 'photo',
+            orientation: 'horizontal',
+            safesearch: true,
+            page: this.page,
+            per_page: this.per_page,
+          },
+        }
+      );
+      this.incrementPageNum();
+      return data;
+    } catch (err) {
+      return Notify.failure(err.message + ` Please, try again later`);
+    }
+  };
   incrementPageNum() {
     this.page += 1;
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import PhotoApiService from './getPhotoApi';
 import { Notify } from 'notiflix';
-import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const formEl = document.querySelector('#search-form');
 const galleryListEl = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
 let allHits = 0;
 const photoApiService = new PhotoApiService();
+let gallery = new SimpleLightbox('.photo-card a', {
+  loop: false,
+});
 
 formEl.addEventListener('submit', onSubmitForm);
 loadMoreBtn.addEventListener('click', onLoadMore);
@@ -27,7 +31,7 @@ function onSubmitForm(e) {
   photoApiService.resetPageNum();
   photoApiService.getPhotos().then(({ hits, totalHits }) => {
     allHits = hits.length;
-    if (hits.length >= totalHits) {
+    if (hits.length > totalHits) {
       Notify.info("We're sorry, but you've reached the end of search results.");
     }
     if (totalHits === 0) {
@@ -36,6 +40,7 @@ function onSubmitForm(e) {
       );
       return;
     } else createPhotoMarkup(hits);
+    gallery.refresh();
     Notify.success(`Hooray! We found ${totalHits} images.`);
     if (!totalHits <= photoApiService.per_page) {
       loadMoreBtn.classList.remove('is-hidden');
@@ -46,7 +51,7 @@ function onSubmitForm(e) {
 function onLoadMore() {
   photoApiService.getPhotos().then(({ hits, totalHits }) => {
     createPhotoMarkup(hits);
-
+    gallery.refresh();
     if (hits.length + allHits >= totalHits) {
       Notify.info("We're sorry, but you've reached the end of search results.");
       loadMoreBtn.classList.add('is-hidden');
@@ -60,8 +65,8 @@ function createPhotoMarkup(photos) {
     photos
       .map(photo => {
         return ` <div class="photo-card">
-      <img src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy"  />
-
+      <a href="${photo.largeImageURL}"><img src="${photo.webformatURL}" alt="${photo.tags}" loading="lazy"  />
+</a>
       <div class="info">
         <p class="info-item">
           <b>Likes</b>
